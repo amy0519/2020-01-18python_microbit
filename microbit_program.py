615517c1e2aa09d353d88e9c560b14c0e3001841
amy0519 <amyis0438@gmail.com>
2020-01-16 15:08:54 +0800

    廣播
    若其他接收器收到會顯示hello

---
+++ microbit_program.py
@@ -0,0 +1,14 @@
+# Add your Python code here. E.g.
+from microbit import *
+import radio
+
+radio.on()
+
+while True:
+    if button_a.was_pressed():
+        radio.send('Hi') 
+        
+    incoming = radio.receive()
+    if incoming == 'Hi':
+        display.show('Hello', delay=300, wait=False, loop=False, clear=True)
+        
